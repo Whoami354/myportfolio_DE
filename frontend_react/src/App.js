4f4e35396fe3e95e48f36c80b0b0926499e19aec
Whoami354 <74734994+Whoami354@users.noreply.github.com>
2023-02-21 16:50:32 +0100

    Vorbereitungen abgeschlossen

---
+++ frontend_react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+{/*import {About, Footer, Header, Skills, Testimonial, Work} from './container/';
+import {Navbar} from './component';
+import './App.scss'*/}
+
+const App = () => {
+    return (
+        <h1>App</h1>
+    )
+}
+
+export default App;
