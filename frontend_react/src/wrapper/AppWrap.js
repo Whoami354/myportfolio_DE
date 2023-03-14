84f7a7333df1634e8c5ee9a867412f44cdfc3453
Whoami354 <74734994+Whoami354@users.noreply.github.com>
2023-03-14 14:44:20 +0100

    Feinheiten gemacht

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {NavigationDots, SocialMedia} from '../component';
+import {NavigationDots} from '../component';
 
 const AppWrap = (Component, idName, classNames) => function HOC() {
     return (
         <div id={idName} className = {`app__container ${classNames}`}>
-            <SocialMedia/>
 
             <div className="app__wrapper app__flex">
                 <Component />
